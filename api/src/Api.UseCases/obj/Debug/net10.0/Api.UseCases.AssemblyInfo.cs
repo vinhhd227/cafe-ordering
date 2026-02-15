4090857008dbb8ee964a8fcbacf4815b7e3ab13b
vinhhd227 <vinhhd227@gmail.com>
2026-02-16 00:50:44 +0700

    fix: git ignore

--- api/src/Api.UseCases/obj/Debug/net10.0/Api.UseCases.AssemblyInfo.cs
+++ api/src/Api.UseCases/obj/Debug/net10.0/Api.UseCases.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api.UseCases")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2ea0a8135ad23785b54167e01de453fd818a689")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63a1f6d960df3e0125ee707325d887a0121388fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api.UseCases")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api.UseCases")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
